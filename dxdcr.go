affb4601193ab5337dd26b627ddb4ee2c32be1a5
Steve Yen <steve.yen@gmail.com>
2013-09-23 15:45:00 -0700

    Invoking StartTapFeed().

--- dxdcr.go
+++ dxdcr.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/couchbaselabs/go-couchbase"
+	"github.com/dustin/gomemcached/client"
 )
 
 var sourceUrl = flag.String("sourceUrl", "http://127.0.0.1:8091", "Source Couchbase REST URL")
@@ -23,12 +24,22 @@ func main() {
 		log.Fatalf("error: could not connect to sourceUrl: %s, sourceBucket: %s, err: %v",
 			*sourceUrl, sourceBucket, err)
 	}
-	log.Printf("source: %#v", source)
-
 	target, err := couchbase.GetBucket(*targetUrl, "default", *targetBucket)
 	if err != nil {
 		log.Fatalf("error: could not connect to targetUrl: %s, targetBucket: %s, err: %v",
 			*targetUrl, *targetBucket, err)
 	}
+	tapArgs := memcached.TapArguments{
+		Backfill: 0,     // Timestamp of oldest item to send.
+		Dump:     false, // If true, source will disconnect after sending existing items.
+		KeysOnly: false,
+	}
+	tap, err := source.StartTapFeed(&tapArgs)
+	if err != nil {
+		log.Fatalf("error: could not StartTapFeed, err: %v", err)
+	}
+
+	log.Printf("source: %#v", source)
 	log.Printf("target: %#v", target)
+	log.Printf("tap: %#v", tap)
 }

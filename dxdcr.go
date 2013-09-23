cbbf44edc46ad36f94504e24987efa383765e0c6
Steve Yen <steve.yen@gmail.com>
2013-09-23 15:22:21 -0700

    Added source/target cmd-line params.

--- dxdcr.go
+++ dxdcr.go
@@ -5,19 +5,30 @@ import (
 	"log"
 	"os"
 
-	cb "github.com/couchbaselabs/go-couchbase"
+	"github.com/couchbaselabs/go-couchbase"
 )
 
-var url = flag.String("couchbase", "http://127.0.0.1:8091", "Couchbase REST URL")
+var sourceUrl = flag.String("sourceUrl", "http://127.0.0.1:8091", "Source Couchbase REST URL")
+var sourceBucket = flag.String("sourceBucket", "default", "Source bucket")
+var targetUrl = flag.String("targetUrl", "http://127.0.0.1:8091", "Target Couchbase REST URL")
+var targetBucket = flag.String("targetBucket", "default", "Target bucket")
 
 func main() {
 	flag.Parse()
 	log.Printf("%s\n", os.Args[0])
 	flag.VisitAll(func(f *flag.Flag) { log.Printf("  -%s=%s\n", f.Name, f.Value) })
 
-	c, err := cb.Connect(*url)
+	source, err := couchbase.GetBucket(*sourceUrl, "default", *sourceBucket)
 	if err != nil {
-		log.Fatalf("error: could not connect to url: %s, err: %v", *url, err)
+		log.Fatalf("error: could not connect to sourceUrl: %s, sourceBucket: %s, err: %v",
+			*sourceUrl, sourceBucket, err)
 	}
-	log.Printf("c: %#v", c)
+	log.Printf("source: %#v", source)
+
+	target, err := couchbase.GetBucket(*targetUrl, "default", *targetBucket)
+	if err != nil {
+		log.Fatalf("error: could not connect to targetUrl: %s, targetBucket: %s, err: %v",
+			*targetUrl, *targetBucket, err)
+	}
+	log.Printf("target: %#v", target)
 }

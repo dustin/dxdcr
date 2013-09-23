64f0cfbfdcae82c63b4f65881376a1ba21e55774
Steve Yen <steve.yen@gmail.com>
2013-09-23 15:10:27 -0700

    Cmd-line -couchbase=url parameter

--- dxdcr.go
+++ dxdcr.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	cb "github.com/couchbaselabs/go-couchbase"
+)
+
+var url = flag.String("couchbase", "http://127.0.0.1:8091", "Couchbase REST URL")
+
 func main() {
-	panic("can't find towel")
+	flag.Parse()
+	log.Printf("%s\n", os.Args[0])
+	flag.VisitAll(func(f *flag.Flag) { log.Printf("  -%s=%s\n", f.Name, f.Value) })
+
+	c, err := cb.Connect(*url)
+	if err != nil {
+		log.Fatalf("error: could not connect to url: %s, err: %v", *url, err)
+	}
+	log.Printf("c: %#v", c)
 }

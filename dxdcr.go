af385dd7b16fbefb6d0f07ca632c6059150e15e1
Dustin Sallings <dustin@spy.net>
2013-09-24 01:24:48 -0700

    Move flag vars into main
    They were shadowing start params, which is mildly confusing.

--- dxdcr.go
+++ dxdcr.go
@@ -9,12 +9,16 @@ import (
 	"github.com/dustin/gomemcached/client"
 )
 
-var sourceUrl = flag.String("sourceUrl", "http://127.0.0.1:8091", "Source Couchbase REST URL")
-var sourceBucket = flag.String("sourceBucket", "default", "Source bucket")
-var targetUrl = flag.String("targetUrl", "http://127.0.0.1:8091", "Target Couchbase REST URL")
-var targetBucket = flag.String("targetBucket", "default", "Target bucket")
-
 func main() {
+	sourceUrl := flag.String("sourceUrl", "http://127.0.0.1:8091",
+		"Source Couchbase REST URL")
+	sourceBucket := flag.String("sourceBucket", "default",
+		"Source bucket")
+	targetUrl := flag.String("targetUrl", "http://127.0.0.1:8091",
+		"Target Couchbase REST URL")
+	targetBucket := flag.String("targetBucket", "default",
+		"Target bucket")
+
 	flag.Parse()
 	log.Printf("%s\n", os.Args[0])
 	flag.VisitAll(func(f *flag.Flag) { log.Printf("  -%s=%s\n", f.Name, f.Value) })
